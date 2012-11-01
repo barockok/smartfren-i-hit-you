2410b36c60bd57fe6e5da16c965cb82371a88408
Zidni Mubarock <zidmubarock@gmail.com>
2012-11-02 02:31:09 +0700

    init dude

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SmartfrenIHitYou::Application.config.secret_token = '29703f8ee775805091f214a6711540b41bc7fe602f4d1767f70d1a0d1baa0a644ad5f61295598e328f6af3b553a078dd413b5cee30f00ed3aff38c82ee87aa93'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SmartfrenIHitYou::Application.config.session_store :cookie_store, key: '_smartfren_i_hit_you_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SmartfrenIHitYou::Application.config.session_store :active_record_store
