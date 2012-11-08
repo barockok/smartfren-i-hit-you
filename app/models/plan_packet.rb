281f65628e28b5298fee59f08227776072d59d0e
Zidni Mubarock <zidmubarock@gmail.com>
2012-11-09 01:20:12 +0700

    nightly test

--- app/models/plan_packet.rb
+++ app/models/plan_packet.rb
@@ -1,6 +1,7 @@
 class PlanPacket < ActiveRecord::Base
   attr_accessible :code, :client_number_id, :will_subscribe_at, :archive
   belongs_to :client_number
+  has_one :active_packet 
   def self.check_apply
     current_time = Time.now
     PlanPacket.where('will_subscribe_at < :current_time or will_subscribe_at = :current_time', {:current_time => current_time})
@@ -9,6 +10,6 @@ class PlanPacket < ActiveRecord::Base
     end
   end
   def apply
-    ActivePacket.create(self.as_json(:only => [:code, :client_number_id]).merge({:plan_packet_id => id, :code => code }) ) if client_number.buy(code)
+    ActivePacket.create(self.as_json(:only => [:code, :client_number_id]).merge({:plan_packet_id => id, :code => code , :will_expired => client_number.api.info[:expired_at] }) ) if client_number.buy(code)
   end
 end
--- spec/models/plan_packet_spec.rb
+++ spec/models/plan_packet_spec.rb
@@ -15,11 +15,18 @@ describe PlanPacket do
       @plan_packet.client_number.nomer.should == @clinet_number.nomer
     end
   end
-  context "Mock SF Client buy" do
-    it("should fine"){
-       Smartfren::Client.any_instance.expects(:buy).returns(true)
-       @sf = Smartfren.connect('XXX', 'XXXX')
-       @sf.buy
+  context "Mock Smartfren::Client " do
+    context("should fine"){
+      it("when buy"){
+         Smartfren::Client.any_instance.expects(:buy).returns(true)
+         @sf = Smartfren.connect('XXX', 'XXXX')
+         @sf.buy
+       }
+       it("when info"){
+         Smartfren::Client.any_instance.expects(:info).returns({:expired_at => Time.now+1.day})
+         @sf = Smartfren.connect('XXX', 'XXXX')
+         @sf.info
+       }
     }
   end
   context 'when check apply' do
@@ -29,6 +36,8 @@ describe PlanPacket do
         @plan_packet.update_attribute(:will_subscribe_at , Time.now)
         #stub Smartfren::Client instance buy
         Smartfren::Client.any_instance.stubs(:buy).returns(true)
+        @time= Time.now+1.day
+        Smartfren::Client.any_instance.stubs(:info).returns({:expired_at => @time})
         PlanPacket.check_apply
       }
       
@@ -38,6 +47,9 @@ describe PlanPacket do
       context "then have one active packet" do
         it "should belongs to right planpacket" do
           ActivePacket.find_by_plan_packet_id(@plan_packet.id).plan_packet.id.should == @plan_packet.id
+        end
+        it "should have active packet with right will_expired time" do
+           @plan_packet.active_packet.will_expired.should == @time
         end
       end
     end
